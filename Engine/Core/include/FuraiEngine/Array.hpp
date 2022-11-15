2f659e87da73db128e0fc5bb60e9848373f66b67
itota37 <40158522+itota37@users.noreply.github.com>
2022-11-16 00:23:49 +0900

    配列を作成しました。 関数オブジェクトを追加しました。 基本例外を追加しました。

--- Engine/Core/include/FuraiEngine/Array.hpp
+++ Engine/Core/include/FuraiEngine/Array.hpp
@@ -7,7 +7,9 @@
 
 #include <initializer_list>
 #include "FuraiEngine/String.hpp"
+#include "FuraiEngine/Errors.hpp"
 #include "FuraiEngine/Allocation.hpp"
+#include "FuraiEngine/Functional.hpp"
 
 /// FuraiEngineの機能を提供する名前空間です。
 namespace FuraiEngine
@@ -22,6 +24,66 @@ namespace FuraiEngine
         USize m_count;  // 要素数です。
         A m_allocator;  // アロケータです。
         T *m_buffer;    // 要素のバッファです。
+
+        /// 要素を別バッファに移動させます。
+        /// @param oldBuffer 移動元のバッファです。
+        /// @param fromIndex 移動元の開始位置です。
+        /// @param newBuffer 移動先のバッファです。
+        /// @param toIndex 移動先の開始位置です。
+        /// @param count 要素数です。
+        void moveElements(T *oldBuffer, USize fromIndex, T *newBuffer, USize toIndex, USize count) noexcept
+        {
+            for (USize i = toIndex, j = fromIndex; i < count; i++, j++)
+            {
+                newBuffer[i] = std::move(oldBuffer[j]);
+            }
+        }
+
+        /// 後ろから順に要素を別バッファに移動させます。
+        /// @param oldBuffer 移動元のバッファです。
+        /// @param fromIndex 移動元の開始位置です。
+        /// @param newBuffer 移動先のバッファです。
+        /// @param toIndex 移動先の開始位置です。
+        /// @param count 要素数です。
+        void moveElementsRev(T *oldBuffer, USize fromIndex, T *newBuffer, USize toIndex, USize count) noexcept
+        {
+            for (ISize i = toIndex + count - 1, j = fromIndex + count - 1; i >= toIndex; i--, j--)
+            {
+                newBuffer[i] = std::move(oldBuffer[j]);
+            } 
+        }
+
+        /// バッファを2倍に拡張します。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void expandBuffer()
+        {
+            // 作成します。
+            USize newLength = this->m_length * 2;
+            auto newBuffer = this->m_allocator.allocate(newLength);
+            // 移動します。
+            this->moveElements(this->m_buffer, 0, newBuffer, 0, this->m_count);
+            // 古いデータを削除します。
+            this->m_allocator.deallocate(this->m_buffer, this->m_length);
+            // 上書きします。
+            this->m_length = newLength;
+            this->m_buffer = newBuffer;
+        }
+
+        /// バッファを半分に縮小します。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void reducedBuffer()
+        {
+            // 作成します。
+            USize newLength = static_cast<USize>(this->m_length * 0.5f);
+            auto newBuffer = this->m_allocator.allocate(newLength);
+            // 移動します。
+            this->moveElements(this->m_buffer, 0, newBuffer, 0, this->m_count);
+            // 古いデータを削除します。
+            this->m_allocator.deallocate(this->m_buffer, this->m_length);
+            // 上書きします。
+            this->m_length = newLength;
+            this->m_buffer = newBuffer;
+        }
 
     public:
 
@@ -123,6 +185,207 @@ namespace FuraiEngine
         ~Array()
         {
             this->m_allocator.deallocate(this->m_buffer, this->m_length);
+        }
+
+        /// 添え字からアクセスします。
+        /// @param index アクセスする位置を指す添え字です。
+        /// @return 要素の参照です。
+        /// @exception BufferOverException 指定位置は配列の範囲外の可能性があります。
+        T &operator[](USize index)
+        {
+            if (index >= this->m_count) throw BufferOverException(TXT("指定位置は配列の範囲外です。"));
+
+            return this->m_buffer[index];
+        }
+
+        /// 添え字からアクセスします。
+        /// @param index アクセスする位置を指す添え字です。
+        /// @return 要素の参照です。
+        /// @exception BufferOverException 指定位置は配列の範囲外の可能性があります。
+        const T &operator[](USize index) const
+        {
+            if (index >= this->m_count) throw BufferOverException(TXT("指定位置は配列の範囲外です。"));
+
+            return this->m_buffer[index];
+        }
+
+        /// 添え字からアクセスします。
+        /// @param index アクセスする位置を指す添え字です。
+        /// @return 要素の参照です。
+        /// @exception BufferOverException 指定位置は配列の範囲外の可能性があります。
+        T &at(USize index)
+        {
+            return (*this)[index];
+        }
+
+        /// 添え字からアクセスします。
+        /// @param index アクセスする位置を指す添え字です。
+        /// @return 要素の参照です。
+        /// @exception BufferOverException 指定位置は配列の範囲外の可能性があります。
+        const T &at(USize index) const
+        {
+            return (*this)[index];
+        }
+
+        /// 末尾に要素を追加します。
+        /// @param value 追加要素です。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void add(const T &value)
+        {
+            // 配列長と要素数が一致した場合、バッファを拡張します。
+            if (this->m_count == this->m_length)
+            {
+                this->expandBuffer();
+            }
+
+            // 追加します。
+            this->m_buffer[this->m_count] = value;
+            this->m_count += 1;
+        }
+
+        /// 末尾に要素を追加します。
+        /// @param value 追加要素です。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void add(T &&value)
+        {
+            // 配列長と要素数が一致した場合、バッファを拡張します。
+            if (this->m_count == this->m_length)
+            {
+                this->expandBuffer();
+            }
+
+            // 追加します。
+            this->m_buffer[this->m_count] = std::move(value);
+            this->m_count += 1;
+        }
+
+        /// 指定位置に要素を挿入します。
+        /// @param index 追加位置です。
+        /// @param value 追加要素です。
+        /// @exception BufferOverException 追加位置が配列の範囲外の可能性があります。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void insert(USize index, const T &value)
+        {
+            if (index < this->m_count) throw BufferOverException(TXT("追加位置が配列の範囲外です。"));
+
+            // 配列長と要素数が一致した場合、バッファを拡張します。
+            if (this->m_count == this->m_length)
+            {
+                this->expandBuffer();
+            }
+
+            // 要素を移動させます。
+            this->moveElementsRev(this->m_buffer, index, this->m_buffer, index + 1, this->m_count);
+
+            // 追加します。
+            this->m_buffer[index] = value;
+            this->m_count += 1;
+        }
+        
+        /// 指定位置に要素を挿入します。
+        /// @param index 追加位置です。
+        /// @param value 追加要素です。
+        /// @exception BufferOverException 追加位置が配列の範囲外の可能性があります。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void insert(USize index, T &&value)
+        {
+            if (index < this->m_count) throw BufferOverException(TXT("追加位置が配列の範囲外です。"));
+
+            // 配列長と要素数が一致した場合、バッファを拡張します。
+            if (this->m_count == this->m_length)
+            {
+                this->expandBuffer();
+            }
+
+            // 要素を移動させます。
+            this->moveElementsRev(this->m_buffer, index, this->m_buffer, index + 1, this->m_count);
+
+            // 追加します。
+            this->m_buffer[index] = std::move(value);
+            this->m_count += 1;
+        }
+
+        /// 指定位置の要素を削除します。
+        /// @param index 削除位置です。
+        /// @exception BufferOverException 追加位置が配列の範囲外の可能性があります。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void removeAt(USize index)
+        {
+            if (index < this->m_count) throw BufferOverException(TXT("削除位置が配列の範囲外です。"));
+
+            // 要素を移動させます。
+            this->moveElements(this->m_buffer, index + 1, this->m_buffer, index, this->m_count);
+            this->m_count -= 1;
+
+            // 要素数が配列長の半分未満の場合、バッファを縮小します。
+            if (this->m_count < static_cast<USize>(this->m_length * 0.5f))
+            {
+                this->reducedBuffer();
+            }
+        }
+
+        /// はじめに見つかった要素の位置を取得します。
+        /// @param value 捜索対象の要素です。
+        /// @param equal 等価を比較する関数です。
+        /// @return はじめに見つかった要素の位置です。
+        /// @retval -1 見つかりませんでした。
+        ISize indexOf(const T &value, const Func<Bool(const T &, const T &)> &equal = [](const T &l, const T &r){ return l == r; }) const noexcept
+        {
+            for (USize i = 0; i < this->m_count; i++)
+            {
+                if (equal(value, this->m_buffer[i]))
+                {
+                    return static_cast<ISize>(i);
+                }
+            }
+            return -1;
+        }
+
+        /// 指定要素を削除します。
+        /// @param value 削除する要素です。
+        /// @exception MemoryException メモリの確保に失敗する可能性があります。
+        void remove(const T &value)
+        {
+            ISize index = this->indexOf(value);
+            if (index != -1)
+            {
+                this->removeAt(static_cast<USize>(index));
+            }
+        }
+
+        /// 要素数を取得します。
+        /// @return 要素数です。
+        USize count() const noexcept
+        {
+            return this->m_count;
+        }
+
+        /// 先頭の可変イテレータを取得します。
+        /// @return 先頭イテレータです。
+        PointerIterator<T> begin() noexcept
+        {
+            return PointerIterator<T>(&this->m_buffer[0]);
+        }
+
+        /// 先頭の不変イテレータを取得します。
+        /// @return 先頭イテレータです。
+        ConstPointerIterator<T> begin() const noexcept
+        {
+            return ConstPointerIterator<T>(&this->m_buffer[0]);
+        }
+        
+        /// 番兵の可変イテレータを取得します。
+        /// @return 番兵イテレータです。
+        PointerIterator<T> begin() noexcept
+        {
+            return PointerIterator<T>(&this->m_buffer[this->m_count]);
+        }
+        
+        /// 番兵の不変イテレータを取得します。
+        /// @return 番兵イテレータです。
+        ConstPointerIterator<T> begin() const noexcept
+        {
+            return ConstPointerIterator<T>(&this->m_buffer[this->m_count]);
         }
     };
 }
